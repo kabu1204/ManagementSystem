0f76aeb07e3171df101f1d3c226721fd88c018ce
kabu1204 <201930344367@mail.scut.edu.cn>
2020-05-16 01:40:39 +0800

    通过QSqlTableModel成功用tableView展示个人信息，并且可以编辑和提交更改。 但是QSqlTableModel只能同时在一张表上进行操作，而它的父类QSqlQueryModel更加灵活，但是不提供编辑功能。 下一步计划重写QSqlQueryModel::data()和::flag()方法实现可编辑。

---
+++ contacts.h
@@ -0,0 +1,11 @@
+#ifndef CONTACTS_H
+#define CONTACTS_H
+
+
+class contacts
+{
+public:
+    contacts();
+};
+
+#endif // CONTACTS_H
---
+++ readonlydelegate.h
@@ -0,0 +1,11 @@
+#ifndef READONLYDELEGATE_H
+#define READONLYDELEGATE_H
+
+
+class readonlyDelegate : public QItemDelegate
+{
+public:
+    readonlyDelegate();
+};
+
+#endif // READONLYDELEGATE_H
